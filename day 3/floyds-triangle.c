4aef945d1d35b76c10428151a68dec73e3b695cb
Chezter Reblando <clc.reblando@gmail.com>
2024-07-19 01:01:54 +0800

    day 3

---
+++ day 3/floyds-triangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int n=1, r;
+    printf("Number of rows: ");
+    scanf("%d", &r);
+    for(int i=1; i<r; i++){
+        for(int j=1; j<=i;j++){
+            printf("%d ", n);
+            ++n;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
